0984b3bdbe286aab07028927dcf8d1c42e2bf84b
Sohail Haqyar <haqyarsohail@gmail.com>
2020-01-02 07:21:48 +0200

    upadte menu-item to navigate to withrouter HOC '

--- king-site/src/App.js
+++ king-site/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-function App() {
-	return (
-		<div>
-			<HomePage />
+import { Route, Switch , BrowserRouter } from 'react-router-dom'
+
+const HatsPage = () => {
+	return(
+	<div>
+		<h1>Hats Page</h1>
+	</div>
+	)
+}
+
+
+const App = ()  =>{
+  return (
+    <div>
+			<Switch>
+     <Route exact path='/' component={HomePage}/>
+     <Route path='/hats' component={HatsPage}/>
+		 </Switch>
 		</div>
-	);
+  );
 }
 
 export default App;
--- king-site/src/menu-item/menu-item.component.jsx
+++ king-site/src/menu-item/menu-item.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./menu-item.styles.scss";
-const MenuItem = ({ title, imageUrl, size }) => {
+import { withRouter } from "react-router-dom";
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
   return (
-    <div
-      className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div
         className="background-image"
         style={{
@@ -11,12 +11,11 @@ const MenuItem = ({ title, imageUrl, size }) => {
         }}
       />
       <div className="content">
-        <h1 className="title">
-          {title.toUpperCase()}
-        </h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop Now</span>
       </div>
     </div>
   );
 };
-export default MenuItem;
+export default withRouter(MenuItem);
+
