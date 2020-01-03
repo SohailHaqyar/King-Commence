f9fdf27deb32e44f69aeeecd9b11332e61af0a8d
Sohail Haqyar <haqyarsohail@gmail.com>
2020-01-03 07:33:45 +0200

    Adding header components and nav bar sorta

--- king-site/src/App.js
+++ king-site/src/App.js
@@ -3,11 +3,12 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch , BrowserRouter } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import ShopData from "./pages/shop/shop.data";
-
+import Header from './components/header/header.component'
 
 const App = ()  =>{
   return (
     <div>
+      <Header/>
 			<Switch>
      <Route exact path='/' component={HomePage}/>
      <Route path='/shop' component={ShopPage}/>
