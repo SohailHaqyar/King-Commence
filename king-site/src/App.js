2bec86cc48f891fa4dff4deb222634c09cb89308
Sohail <haqyarsohail@gmail.com>
2020-01-05 10:59:06 +0200

    Integrated firebase sign in and out

--- king-site/src/App.js
+++ king-site/src/App.js
@@ -1,20 +1,39 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import { Route, Switch , BrowserRouter } from 'react-router-dom'
-import ShopPage from './pages/shop/shop.component'
-import ShopData from "./pages/shop/shop.data";
-import Header from './components/header/header.component'
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
+import { auth } from "./firebase/firebase.utils";
 
-const App = ()  =>{
-  return (
-    <div>
-      <Header/>
-			<Switch>
-     <Route exact path='/' component={HomePage}/>
-     <Route path='/shop' component={ShopPage}/>
-		 </Switch>
-		</div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null
+    };
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    auth.onAuthStateChanged(user => this.setState({ currentUser: user }));
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
