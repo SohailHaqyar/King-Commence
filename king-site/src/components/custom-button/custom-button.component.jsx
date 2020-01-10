514d64c39f728bd2c653dd29be87641421a2242c
Sohail <haqyarsohail@gmail.com>
2020-01-10 08:55:37 +0200

    Added a quantity thing cart utils updated to group items in cart reducer

--- king-site/src/components/custom-button/custom-button.component.jsx
+++ king-site/src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import './custom-button.styles.scss'
+import "./custom-button.styles.scss";
 
-const CustomButton = ( { children , isGoogleSignIn, ...otherProps } ) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? "inverted" : ""} custom-button`}
+    {...otherProps}
+  >
     {children}
   </button>
-)
-export default CustomButton
+);
+export default CustomButton;
